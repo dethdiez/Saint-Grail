db883b98243d25ae5cf763e8cfc53e826152f80a
dethdiez <betep9999@yandex.ru>
2016-04-27 01:47:52 +0300

    trying to instantiate prefabs

---
+++ project/Saint-Grail/Assets/Structure/data/scenes/free/FieldCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldCreator : MonoBehaviour {
+
+	public GameObject[] enemy;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i<2; i++) {
+			GameObject enemyClone;
+			Vector3 pos = new Vector3 (5 * i, i, 0);
+			enemyClone = Instantiate (enemy[i], pos, Quaternion.identity) as GameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
