ce152fbfee4476e49cfe70a5d8fe61d90c6bdd72
dethdiez <betep9999@yandex.ru>
2016-05-17 00:09:00 +0300

    sth

---
+++ project/Saint-Grail/Assets/Structure/system/Scene/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void quit() {
+		Application.Quit ();
+	}
+}
