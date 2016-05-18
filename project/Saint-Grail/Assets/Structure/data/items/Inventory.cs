62aad62337a5dc9c307668fcd071257e1dd60b71
dethdiez <betep9999@yandex.ru>
2016-05-18 05:22:34 +0300

    inventory

---
+++ project/Saint-Grail/Assets/Structure/data/items/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum ItemName {energy, health, mushroom, drop, bone, gold, scroll, enchantP, enchantJ, enchantK}
+
+public class Inventory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		item = new int[10];
+		GameObject[] o = GameObject.FindGameObjectsWithTag ("Item");
+		for (int i = 0; i<10; i++) {
+			item [i] = 0;
+			Debug.Log ("Start Inventory. object " + i + " named " + o [i].name);
+			setItemAmount (o [i], 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject[] o = GameObject.FindGameObjectsWithTag ("Item");
+		for (int i = 0; i<10; i++) {
+			if(isExist(i))
+				setItemAmount(o[i], item[i]);
+		}
+	}
+
+	public static void updItemAmount (int name, int upd) {
+		item [name] += upd;
+	}
+
+	public static void setItemAmount (GameObject obj, int amount) {
+		obj.GetComponentInChildren<Text> ().text = amount.ToString ();
+	}
+
+	public static bool isExist(int name) {
+		return (item [name] > 0);
+	}
+
+	private static int[] item;
+}
